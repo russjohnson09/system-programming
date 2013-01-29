a05e8d13abcd45e77c82a8cc4f7487950ee10d21
russjohnson09 <russjohnson09@gmail.com>
2013-01-29 16:26:53 -0500

    lab3 done

--- cipher/cipher.c
+++ cipher/cipher.c
@@ -6,39 +6,47 @@
 #include <stdlib.h>
 
 /*Load array given with the letter frequencies for English from file LetFreq.txt */
-
 void readFreq ( float given[] );
  
 
 /* Read the encoded text from an input file and accumulate the letter frequency data for the encoded text. Store the frequency data in array found. */
-
 void calcFreq ( float found[] );
 
 /* Compare the data in array found with the frequency data in array given, looking for a key that will give you the best match. To do this, try each of the 26 rotations, and remember which gives the smallest difference between the frequencies you observed and the frequencies given. Return the key. */
 int findKey ( float given[], float found[] );
 
- 
-
 /* Decrypt the encoded text in the input file using the key and display the decoded text */
-
 void decrypt ( int key );
 
 /**
 	Decrypts a character using the Caesar cipher.
-	@param ch the character to encrypt
-	@param k the encryption key
+	@param key the encryption key
 	@return the encrypted character
 */
 void decrypt(int key)
 {
 }
+/*
+void readFreq(float given[]) {
+
+    char ch;
+    float f;
+
+    while (fscanf(fin, "%c", &ch) != EOF) {
+        printf("%c",ch);
+    }
+}
+*/
 
 int main(int argc, char* argv[])
 {  
 
-    char ch;
     /*fin encryted file fout decrypted file*/
-	FILE *fin, *fout;
+    FILE *fin, *fout;
+
+    float given[26];
+    float found[26];
+    char ch;
 	
     fin = fopen(argv[1], "r");
 	fout = fopen(argv[2], "w");
@@ -48,15 +56,11 @@ int main(int argc, char* argv[])
 		exit(1);
 	}
 
-	while (fscanf(fin, "%c", &ch) != EOF)
-	{
-		if (isalpha(ch)) {
-            if (islower(ch)) {
-            }
-            else {
-            }
-        }
-	}
+    while (fscanf(fin, "%c", &ch) != EOF) {
+        printf("%c", ch);
+    }
+
+//    readFreq(given);
 
 	fclose(fin);
 	fclose(fout);
---
+++ lab3/lab3.c
@@ -0,0 +1,80 @@
+// lab3.c - measure execution time of C code
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+void printTime(clock_t,clock_t);
+
+int main ()
+{
+	const int MaxSize = 25000;
+	int i, j, temp;
+
+	clock_t begin1;
+    clock_t end1;
+    clock_t begin2;
+    clock_t end2;
+
+	// Part one: processing a statically allocated array
+
+    begin1 = clock();
+
+	int staticArray[MaxSize];		// declare an array 
+
+	for (i=0; i<MaxSize; i++)		// initialize the array with a
+		staticArray[i] = MaxSize-i;	// descending sequence of values
+
+	for (i=0; i<MaxSize-1; i++)		// bubble sort data in the array
+		for (j=MaxSize-1; j>i; j--)
+			if (staticArray[j-1] > staticArray[j])
+			{
+				temp = staticArray[j-1];
+				staticArray[j-1] = staticArray[j];
+				staticArray[j] = temp;
+			}
+
+    end1 = clock();
+
+
+	// Part two: processing a dynamically allocated array
+
+    begin2 = clock();
+
+	float * list;
+	list = (float*) malloc (MaxSize * sizeof (float));
+	
+	for ( i = 0; i < MaxSize; i++ )
+		list[i] = MaxSize-i;
+
+	for (i=0; i<MaxSize-1; i++)		// bubble sort data in the array
+		for (j=MaxSize-1; j>i; j--)
+			if (list[j-1] > list[j])
+			{
+				temp = list[j-1];
+				list[j-1] = list[j];
+				list[j] = temp;
+			}
+
+
+	free(list);
+
+    end2 = clock();
+
+	// Display the amount of time used for each part above
+
+    
+    printTime(begin1, end1);
+
+    printTime(begin2, end2);
+
+
+	return 0;
+}
+
+void printTime(clock_t begin, clock_t end) {
+   printf("Time used: %lf%s\n",(double)(end-begin)/CLOCKS_PER_SEC,
+            " seconds");
+}
+
+
